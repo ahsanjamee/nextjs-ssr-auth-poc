eb7dcb703bcd7819559b0ad165378abd88a7823e
Shahriar Shojib <shahriar_shojib@hotmail.com>
2021-07-21 02:53:57 +0600

    fixed auth logic

--- apps/api/src/auth/services/user.service.ts
+++ apps/api/src/auth/services/user.service.ts
@@ -1,7 +1,7 @@
 import { JwtService } from '@auth-demo/jwt';
 import { HttpException, Injectable } from '@nestjs/common';
 import { ReturnModelType } from '@typegoose/typegoose';
-import { compareSync, hashSync } from 'bcryptjs';
+import { compareSync } from 'bcryptjs';
 import { InjectModel } from 'nestjs-typegoose';
 import { CreateUserDTO } from '../dto/create-user.dto';
 import { UserEntity } from '../entities/user.entity';
@@ -21,12 +21,11 @@ export class UserService {
 	async login(user: CreateUserDTO) {
 		const doc = await this.userModel.findOne({ name: user.name });
 		if (!doc) throw new HttpException('Invalid Username Or Password', 401);
-		const crypted = hashSync(user.password, 8);
-		const isValid = compareSync(crypted, doc.password);
+		const isValid = compareSync(user.password, doc.password);
 		if (isValid) {
 			const token = this.jwtService.sign({ id: doc.id, name: doc.name });
-			return token;
+			return { token };
 		}
-		if (!doc) throw new HttpException('Invalid Username Or Password', 401);
+		throw new HttpException('Invalid Username Or Password', 403);
 	}
 }
